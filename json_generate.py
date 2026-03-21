c56329025e7fb76fbc4b6d9d4ce244d7fc046517
Aaron Davis <aaron.m.davis@comcast.net>
2026-03-20 21:28:23 -0500

    made json_generate a library

--- json_generate.py
+++ json_generate.py
@@ -1,14 +1,17 @@
 import json
 import random
 
-with open('lot_data.json', 'r') as file:
-    data = json.load(file)
+def update_json(original_json: str = 'lot_data.json', dest_json: str = 'data.json'):
+    with open(original_json, 'r') as file:
+        data = json.load(file)
 
-for lot in data["parking_lots"]:
-    lot["occupancy"] = random.randrange(0, lot["capacity"])
+    for lot in data["parking_lots"]:
+        lot["occupancy"] = random.randrange(0, lot["capacity"])
 
-output = json.dumps(data, indent=2)
-print(output)
+    output = json.dumps(data, indent=2)
+    print(output)
 
-with open("data.json", "w") as file:
-    json.dump(data, file, indent=2)
+    with open(dest_json, "w") as file:
+        json.dump(data, file, indent=2)
+
+update_json()
