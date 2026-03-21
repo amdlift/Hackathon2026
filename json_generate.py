ea266c7eb22df3faa267fb3519e580acb000a847
Prabenjan Mayalagu <pm0066@uah.edu>
2026-03-21 16:32:24 -0500

    generate python dict with camera data

--- json_generate.py
+++ json_generate.py
@@ -1,11 +1,17 @@
 import json
 import random
 
-def update_json(original_json: str = 'lot_data.json', dest_json: str = 'data.json'):
-    with open(original_json, 'r') as file:
+def update_json(lot_counts, original_json="lot_data.json"):
+    with open(original_json, "r") as file:
         data = json.load(file)
 
     for lot in data["parking_lots"]:
-        lot["occupancy"] = random.randrange(0, lot["capacity"])
+        lot_code = lot["lot"]
+        capacity = lot["capacity"]
+
+        if lot_code in lot_counts:
+            lot["occupancy"] = lot_counts[lot_code]
+        else:
+            lot["occupancy"] = random.randrange(0, capacity)
 
     return data
