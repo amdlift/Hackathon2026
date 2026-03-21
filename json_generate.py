6bbdc25bfc1f41146e03cceeaf3d694f802355b5
Aaron Davis <aaron.m.davis@comcast.net>
2026-03-20 21:05:18 -0500

    added pseudo occupancy data generation

--- json_generate.py
+++ json_generate.py
@@ -1,14 +1,10 @@
 import json
 
-def generate_json(lot_name: str = "A1", num_spots: int = 100, current: int = 50):
-    x = {
-        "lot_name": lot_name,
-        "max_occupancy": num_spots,
-        "current_occupancy": current
-    }
+with open('lot_data.json', 'r') as file:
+    data = json.load(file)
 
-    y = json.dumps(x, indent=2)
+for lot in data["parking_lots"]:
+    lot["occupancy"] = 20
 
-    print(y)
-
-generate_json("B2", 80, 75)
+output = json.dumps(data, indent=2)
+print(output)
